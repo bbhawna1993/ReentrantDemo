7e84664b00d73614d138f86edae9013bea5da54f
bhawna <bhawnabudhiraja1993@gmail.com>
2019-05-05 15:23:57 +0530

    added reentrant locks methods

---
+++ src/ReentrantLockDemo2.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockDemo2 {
+
+	public static void main(String[] args)
+	{
+
+		ReentrantLock l=new ReentrantLock();
+		l.lock();
+		l.lock();
+		System.out.println(l.getHoldCount());
+		System.out.println(l.getQueueLength());
+		System.out.println(l.isHeldByCurrentThread());
+
+		l.unlock();
+		l.unlock();
+
+		System.out.println(l.getHoldCount());
+	}
+
+}
